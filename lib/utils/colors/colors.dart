90f1a8bdcb51bb83c7ed36f26f540b1a32c49582
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-30 20:18:21 +0900

    FEAT: main colors

---
+++ lib/utils/colors/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MainColors {
+  // primary
+  static const Color primary = Color(0xFFFE7B7B);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+}
