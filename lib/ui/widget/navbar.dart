0580ac6b948296dc4c4d92e9e6adbd9c1a76e77c
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-05-07 14:09:15 +0900

    ADD: 주석

--- lib/ui/widget/navbar.dart
+++ lib/ui/widget/navbar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+// 명명된 매개변수로 커스텀 navigation bar
 PreferredSizeWidget? navBar({String? url, IconData? icon}) {
   return PreferredSize(
     preferredSize: const Size.fromHeight(kToolbarHeight),
