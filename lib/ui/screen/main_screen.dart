3e3da6e081ef79c5a7943ff016d16ccd70585da5
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-05-07 13:59:20 +0900

    FEAT: main screen nav

---
+++ lib/ui/screen/main_screen.dart
@@ -0,0 +1,37 @@
+import 'package:fan_app/ui/widget/navbar.dart';
+import 'package:fan_app/utils/colors/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      builder: (context, child) => Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          leading: const Icon(
+            Icons.menu_rounded,
+            color: MainColors.black,
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_vert,
+                color: MainColors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/widget/navigation_bar.dart
+++ lib/ui/widget/navigation_bar.dart
@@ -1,4 +1,5 @@
 import 'package:fan_app/provider/bottom_navigation_provider.dart';
+import 'package:fan_app/ui/screen/main_screen.dart';
 import 'package:flutter/material.dart';
 
 Widget navigationBody(BottomNavigationProvider provider) {
@@ -6,7 +7,7 @@ Widget navigationBody(BottomNavigationProvider provider) {
   return provider.currentPage == 0
       ? const SafeArea(
           child: Center(
-            child: Text("Menu 1"),
+            child: MainScreen(),
           ),
         )
       : provider.currentPage == 1
