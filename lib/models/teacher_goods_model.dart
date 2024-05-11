ae3866a09529c274263dd48a23ae6c7954bd99b1
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-05-11 13:04:25 +0900

    FEAT: model

--- lib/models/teacher_goods_model.dart
+++ lib/models/teacher_goods_model.dart
@@ -1,6 +1,6 @@
 List<Map<String, dynamic>> goods = [
   {
-    "image": "https://www.yeongnam.com/mnt/file/202311/20231121001110427_1.jpg",
+    "image": "",
     "name": "포토카드",
     "price": 1000,
     "teacher": "최병준",
@@ -8,7 +8,13 @@ List<Map<String, dynamic>> goods = [
   {
     "image": "",
     "name": "팬카페 입장표",
-    "price": 1000,
+    "price": 10000,
     "teacher": "차수민",
+  },
+  {
+    "image": "",
+    "name": "소원권",
+    "price": 5000,
+    "teacher": "박건우",
   },
 ];
