ad3adb0cae7b05817d723d082d8b36ad5535716d
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-05-11 13:00:10 +0900

    FIX: model 분리

---
+++ lib/models/teacher_goods_model.dart
@@ -0,0 +1,14 @@
+List<Map<String, dynamic>> goods = [
+  {
+    "image": "https://www.yeongnam.com/mnt/file/202311/20231121001110427_1.jpg",
+    "name": "포토카드",
+    "price": 1000,
+    "teacher": "최병준",
+  },
+  {
+    "image": "",
+    "name": "팬카페 입장표",
+    "price": 1000,
+    "teacher": "차수민",
+  },
+];
--- lib/ui/screen/main_screen.dart
+++ lib/ui/screen/main_screen.dart
@@ -1,3 +1,5 @@
+import 'package:fan_app/models/teacher_goods_model.dart';
+import 'package:fan_app/models/teacher_list_model.dart';
 import 'package:fan_app/ui/widget/goods_list.dart';
 import 'package:fan_app/ui/widget/navbar.dart';
 import 'package:fan_app/ui/widget/teacher_list.dart';
@@ -17,68 +19,6 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  List<Map<String, String>> somaGOD = [
-    {
-      "image":
-          "https://i.namu.wiki/i/ZnBMAAGJaiFKqDmASXCt-977Xuq6gLA-G8AsD4K1BKCVBEzrjISoW9QyfcSKPnacwuBpCGSSyBtCJv8E-UocNQ.webp",
-      "name": "최병준 선생님"
-    },
-    {
-      "image":
-          "https://img.sbs.co.kr/newsnet/etv/upload/2022/02/15/30000743641_1280.jpg",
-      "name": "차수민 선생님"
-    },
-    {
-      "image":
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsJILP5EjF59E4CwEcnqaAjiLaE2Yn2ibPpeWxjdLFOw&s",
-      "name": "박제현 선생님"
-    },
-    {
-      "image":
-          "https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/03/21/d073425d-9762-4c03-b203-814838f33327.jpg",
-      "name": "박건우 선생님"
-    },
-    {
-      "image":
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYXG4f-9uDgL2LsGuP-o_LLPlmgTgX_UQIj451D-3p9w&s",
-      "name": "이세준 선생님"
-    },
-  ];
-
-  List<Map<String, String>> somaNewJeans = [
-    {
-      "image":
-          "https://www.yeongnam.com/mnt/file/202311/20231121001110427_1.jpg",
-      "name": "박민하 선생님"
-    },
-    {
-      "image": "https://i.ytimg.com/vi/LcS-451yDxI/maxresdefault.jpg",
-      "name": "이정하 선생님"
-    },
-    {
-      "image": "https://i.ytimg.com/vi/k3jV6DMTCSE/maxresdefault.jpg",
-      "name": "김상화 선생님"
-    },
-    {
-      "image": "https://i.ytimg.com/vi/SiDYYqj9Knk/maxresdefault.jpg",
-      "name": "진예빈 선생님"
-    },
-    {
-      "image": "https://i.ytimg.com/vi/hWCOlJOySog/maxresdefault.jpg",
-      "name": "이나영 선생님"
-    },
-  ];
-
-  List<Map<String, dynamic>> goods = [
-    {
-      "image":
-          "https://www.yeongnam.com/mnt/file/202311/20231121001110427_1.jpg",
-      "name": "최병준 선생님 포토카드",
-      "price": 1000,
-      "teacher": "최병준",
-    },
-  ];
-
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
