a58e6eee9065a3f72dfdab3d43017c3fa4ccde1e
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-05-19 19:29:52 +0900

    FEAT : 굿즈샵

--- lib/models/teacher_goods_model.dart
+++ lib/models/teacher_goods_model.dart
@@ -1,18 +1,21 @@
 List<Map<String, dynamic>> goods = [
   {
-    "image": "",
+    "image":
+        "https://item.kakaocdn.net/do/5f9b01433b1accf2269d12569ecbb2a64022de826f725e10df604bf1b9725cfd",
     "name": "포토카드",
     "price": 1000,
     "teacher": "최병준",
   },
   {
-    "image": "",
+    "image":
+        "https://item.kakaocdn.net/do/f0087c08a6faae31004e570aad5653cd7154249a3890514a43687a85e6b6cc82",
     "name": "팬카페 입장표",
     "price": 10000,
     "teacher": "차수민",
   },
   {
-    "image": "",
+    "image":
+        "https://item.kakaocdn.net/do/dab76196380443d6903b86ebbba305f7c37d537a8f2c6f426591be6b8dc7b36a",
     "name": "소원권",
     "price": 5000,
     "teacher": "박건우",
--- lib/ui/screen/goods_screen.dart
+++ lib/ui/screen/goods_screen.dart
@@ -1,5 +1,8 @@
+import 'package:fan_app/models/teacher_goods_model.dart';
 import 'package:fan_app/ui/widget/navbar.dart';
+import 'package:fan_app/utils/colors/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
@@ -23,7 +26,93 @@ class _GoodsScreenState extends State<GoodsScreen> {
         body: SingleChildScrollView(
           child: Center(
             child: Column(
-              children: [],
+              children: [
+                Container(
+                  width: 0.9.sw,
+                  margin: EdgeInsets.only(top: 10.0.h),
+                  child: Text(
+                    "굿즈샵 쇼핑하기",
+                    style: TextStyle(
+                      color: MainColors.primary,
+                      fontSize: 20.0.sp,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0.h,
+                ),
+                // GridView.builder를 사용하기 전에 Container로 감싸고, 높이를 지정해줍니다.
+                SingleChildScrollView(
+                  child: GridView.builder(
+                    shrinkWrap: true, // 추가
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10, // 가로 간격
+                      mainAxisSpacing: 10, // 세로 간격
+                    ),
+                    itemCount: goods.length, // 아이템의 총 개수
+                    itemBuilder: (BuildContext context, int index) {
+                      return GestureDetector(
+                        onTap: () {
+                          print("${index + 1}번째 사진");
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(goods[index]["image"]),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Container(
+                                width: 100.w,
+                                color: Colors.white.withOpacity(
+                                  0.9,
+                                ), // 텍스트 배경을 약간 어둡게 하여 가독성 향상
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      goods[index]["name"] ?? "",
+                                      style: const TextStyle(
+                                        color: MainColors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 5.0),
+                                    Text(
+                                      "#${goods[index]["teacher"]}",
+                                      style: const TextStyle(
+                                        color: MainColors.primary,
+                                        fontSize: 18.0,
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 5.0),
+                                    Text(
+                                      "${goods[index]["price"].toString()}p",
+                                      style: const TextStyle(
+                                        color: MainColors.black,
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
         ),
